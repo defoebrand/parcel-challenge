4895f55308444d083ebc0ddce265d843984e29f5
Brandon <defoe.brand@gmail.com>
2022-03-10 01:27:32 +0200

    Initialize database table and associated model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_09_224659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parcel_machines", force: :cascade do |t|
+    t.string "zip", null: false
+    t.string "name", null: false
+    t.string "type"
+    t.jsonb "a_name", default: {"0"=>"", "1"=>"", "2"=>"", "3"=>"", "4"=>"", "5"=>"", "6"=>"", "7"=>"", "8"=>""}
+    t.jsonb "coordinates", default: {"X"=>"", "Y"=>""}
+    t.string "service_hours"
+    t.string "temp_service_hours"
+    t.string "temp_service_hours_until"
+    t.string "temp_service_hours_2"
+    t.string "temp_service_hours_2_until"
+    t.jsonb "comment", default: {"eng"=>"", "est"=>"", "lav"=>"", "lit"=>"", "rus"=>""}
+    t.string "modified"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
