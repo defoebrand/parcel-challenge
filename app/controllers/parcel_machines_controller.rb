0ba95fa6ce783d9528987bf56a7854bcfaf716e2
Brandon <defoe.brand@gmail.com>
2022-03-10 05:39:53 +0200

    Create export to Excel functionality

--- app/controllers/parcel_machines_controller.rb
+++ app/controllers/parcel_machines_controller.rb
@@ -6,4 +6,20 @@ class ParcelMachinesController < ApplicationController
   def show
     @parcel_machine = ParcelMachine.find(params[:id])
   end
+  
+  def export_all
+    @parcel_machines = ParcelMachine.all
+
+    respond_to do |format|
+      format.xlsx
+    end
+  end
+  
+  def export_single
+    @parcel_machines = ParcelMachine.where(id: params[:id])
+
+    respond_to do |format|
+      format.xlsx
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :parcel_machines, only: :show
   root 'parcel_machines#index'
+  resources :parcel_machines, only: :show
+  get '/export_all', to: 'parcel_machines#export_all', as: 'export_all'
+  get '/export_single/:id', to: 'parcel_machines#export_single', as: 'export_single'
 end
