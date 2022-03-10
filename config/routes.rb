e9ca530669999ae5aa4d66745350cbb5528eda8e
Brandon Defoe <defoe.brand@gmail.com>
2022-03-10 10:47:53 +0200

    Cleanup ReadMe / Routes / Styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'parcel_machines#index'
   resources :parcel_machines, only: :show
-  get '/export_all', to: 'parcel_machines#export_all', as: 'export_all'
-  get '/export_single/:id', to: 'parcel_machines#export_single', as: 'export_single'
 end
