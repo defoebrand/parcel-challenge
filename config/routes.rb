3ae2963b1c359cfdf7074de395dba1ebb0d344e9
Brandon <defoe.brand@gmail.com>
2022-03-10 10:47:11 +0200

    Remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'parcel_machines#index'
   resources :parcel_machines, only: :show
-  get '/export_single/:id', to: 'parcel_machines#export_single', as: 'export_single'
 end
