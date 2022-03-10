0112e0f9c19b0049b58efe45c5c20cfe67fe5c96
Brandon <defoe.brand@gmail.com>
2022-03-10 05:46:03 +0200

    Change schedule time from testing parameter to every night

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,6 @@ env :PATH, ENV['PATH']
 
 set :output, "log/cron_log.log"
 
-every 1.minute do 
-    rake 'import:locations'
+every :day, at: '1:30am' do 
+  rake 'import:locations'
 end
-
-# every :day, at: '12am' do 
-#   rake 'import:locations'
-# end
