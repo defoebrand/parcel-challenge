d17c94cefc9a7260cc80b848418001a482a1c7c6
Brandon Defoe <defoe.brand@gmail.com>
2022-03-10 04:37:43 +0200

    Setup Frontend

--- spec/requests/parcel_machines_spec.rb
+++ spec/requests/parcel_machines_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'ParcelMachines' do
     it 'returns successful response' do
       get root_path
 
-      # expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET #show' do
     it 'returns successful response' do
-      get machine_path(machine)
+      get parcel_machine_path(machine)
   
       expect(response).to have_http_status(:success)
     end
